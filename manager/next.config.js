d240ba419f4add074b875e2f449b31acc9e69246
7H0M45-4N70NY <thomasantony14@gmail.com>
2025-12-13 13:09:53 +0530

    CI Added

--- manager/next.config.js
+++ manager/next.config.js
@@ -1,37 +1,39 @@
 const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-    register: true,
-    skipWaiting: true,
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
-    reactCompiler: false,
-    webpack: (config) => {
-        config.watchOptions = {
-            poll: false,
-            aggregateTimeout: 10000,
-        };
-        return config;
-    },
-    async headers() {
-        return [
-            {
-                source: '/sw.js',
-                headers: [
-                    {
-                        key: 'Service-Worker-Allowed',
-                        value: '/',
-                    },
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=0, must-revalidate',
-                    },
-                ],
-            },
-        ];
-    },
+  reactCompiler: false,
+
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: false,
+      aggregateTimeout: 10000,
+    };
+    return config;
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/sw.js",
+        headers: [
+          {
+            key: "Service-Worker-Allowed",
+            value: "/",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = withPWA(nextConfig);
